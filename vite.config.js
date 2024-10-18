84c44557d059d19fc3beae4c11ee9e2cbeff2551
Alex Zhou <azho0003@student.monash.edu>
2024-10-18 21:21:01 +1100

    Added vue.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/FIT5032-WK12/' : '/'
-}
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
